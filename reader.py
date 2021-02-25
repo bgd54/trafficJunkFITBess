d65a1dc42599183835964105281dc66dad5b822f
sulan <sulyok.andras.attila@itk.ppke.hu>
2021-02-25 19:23:37 +0100

    read file

--- reader.py
+++ reader.py
@@ -1,4 +1,5 @@
 import numpy as np
+import fire
 from typing import NamedTuple, List
 
 class Street(NamedTuple):
@@ -16,3 +17,22 @@ class World(NamedTuple):
     bonus_points: int  # given to every car thet reaches destination
     streets: List[Street]
     cars: List[Car]
+
+def read_file(fname: str) -> World:
+    with open(fname, 'r') as f:
+        D, I, S, V, F = [int(a) for a in f.readline().strip().split()]
+        streets = []
+        for _ in range(S):
+            B, E, name, L = f.readline().strip().split()
+            B = int(B)
+            E = int(E)
+            L = int(L)
+            streets.append(Street(B, E, name, L))
+        cars = []
+        for _ in range(V):
+            s = f.readline().strip().split()[1:]
+            cars.append(Car(s))
+        return World(D, I, F, streets, cars)
+
+if __name__ == "__main__":
+    fire.Fire({'read': read_file})
