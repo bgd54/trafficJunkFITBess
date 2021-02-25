6b194029e6ca2b8c1efa4ce56ad6448f030acc38
Árpád Goretity <h2co3@h2co3.org>
2021-02-25 20:15:36 +0100

    isolated subgraphs

--- subgraph.py
+++ subgraph.py
@@ -1,6 +1,7 @@
 #!python3
 
 from typing import List
+from queue import SimpleQueue
 from reader import World, Street, Intersection, read_file
 
 
@@ -19,18 +20,27 @@ def make_graph(world: World) -> List[Intersection]:
 def max_indegree(nodes: List[Intersection]) -> int:
     return max(len(node.in_streets) for node in nodes)
 
-# def has_isolated(nodes: List[Intersection]) -> bool:
-#     visited_intersections = set()
-#
-#     Q = nodes[0]
-#
-#     for neighbor in next_node.out_streets:
-#
-#
-#     return False
+def has_isolated(nodes: List[Intersection]) -> bool:
+    visited_intersections = set()
+
+    Q = SimpleQueue()
+    Q.put(nodes[0])
+
+    while not Q.empty():
+        next_node = Q.get()
+        visited_intersections.add(next_node.id)
+
+        for street in next_node.out_streets:
+            Q.put(nodes[street.end])
+
+    return len(visited_intersections) < len(nodes)
 
 if __name__ == '__main__':
-    for name in ['a', 'b', 'c', 'd', 'e', 'f']:
-        world = read_file('data/{}.txt'.format(name))
-        nodes = make_graph(world)
-        print('MaxIndegree({}) = {}'.format(name, max_indegree(nodes)))
+    world = read_file('data/a.txt')
+    nodes = make_graph(world)
+    print(has_isolated(nodes))
+
+    # for name in ['a', 'b', 'c', 'd', 'e', 'f']:
+    #     world = read_file('data/{}.txt'.format(name))
+    #     nodes = make_graph(world)
+    #     print('MaxIndegree({}) = {}'.format(name, max_indegree(nodes)))
