cfbdf98e4c56537e93431992164ba1e33dcb27d8
sulan <sulyok.andras.attila@itk.ppke.hu>
2021-02-25 19:26:48 +0100

    references are to street objects

--- reader.py
+++ reader.py
@@ -1,6 +1,6 @@
 import numpy as np
 import fire
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Dict
 
 class Street(NamedTuple):
     begin: int  # intersection
@@ -15,19 +15,20 @@ class World(NamedTuple):
     duration: int  # duration of simulation
     num_intersections: int
     bonus_points: int  # given to every car thet reaches destination
-    streets: List[Street]
+    streets: Dict[str, Street]
     cars: List[Car]
 
 def read_file(fname: str) -> World:
     with open(fname, 'r') as f:
         D, I, S, V, F = [int(a) for a in f.readline().strip().split()]
-        streets = []
+        streets: Dict[str, Street] = dict()
         for _ in range(S):
             B, E, name, L = f.readline().strip().split()
             B = int(B)
             E = int(E)
             L = int(L)
-            streets.append(Street(B, E, name, L))
+            assert name not in streets
+            streets[name] = Street(B, E, name, L)
         cars = []
         for _ in range(V):
             s = f.readline().strip().split()[1:]
